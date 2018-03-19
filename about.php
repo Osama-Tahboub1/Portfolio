88025cac398423e352fe471cb37159cf44324938
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-03-19 14:00:04 +0000

    Changed about page from html to Php, also added link to database

--- about.php
+++ about.php
@@ -1,3 +1,14 @@
+<?php
+
+$db = new PDO('mysql:host=127.0.0.1; dbname=osamasCMSDB', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+$query = $db->prepare("SELECT `aboutMeArticle` FROM `aboutMe`;");
+$query->execute();
+$result = $query->fetchAll();
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
