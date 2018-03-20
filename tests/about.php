aeace048502a6b0ecb63c73cd73991e2779d73f6
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-03-20 13:13:54 +0000

    added test for insertParagraph function

--- tests/about.php
+++ tests/about.php
@@ -1,7 +1,17 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 20/03/2018
- * Time: 09:24
- */
+use PHPUnit\Framework\TestCase;
+
+require '../about.php';
+
+class StackTest extends TestCase
+{
+    //Success
+    public function testInsertParagraph()
+    {
+        $expected = '<p>I am currently attending Mayden Academy to become a full stack developer using HTML, CSS, Java Script, PHP, SQL and Node.js.</p><p>Other interests are back end development using ASP.net and C#.</p>';
+        $input = [0 => ["aboutMeArticle"=>  "I am currently attending Mayden Academy to become a full stack developer using HTML, CSS, Java Script, PHP, SQL and Node.js."], 1 => ["aboutMeArticle" => "Other interests are back end development using ASP.net and C#." ]];
+        $case = insertParagraph($input);
+
+        $this->assertEquals($case, $expected);
+    }
+}
