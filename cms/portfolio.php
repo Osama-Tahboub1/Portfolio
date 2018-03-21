4993ab8eab6f8a6a72db4d799f21641b61145dea
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-03-21 20:28:17 +0000

    amended doc blocks

--- cms/portfolio.php
+++ cms/portfolio.php
@@ -26,11 +26,11 @@ $projectInfoQuery->execute();
 $projectInfo = $projectInfoQuery->fetchAll();
 
 /* Doc Block
- * Returns project titles from array content provided by database.
+ * Returns all project titles from array content provided by database.
  *
  * @param $array array associative array provided by database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string a list of project titles in the arrays within the arrays pulled from the database.
  */
 function returnProjectTitles(array $array): string{ // change title to record
     $titles = '';
@@ -41,11 +41,11 @@ function returnProjectTitles(array $array): string{ // change title to record
 }
 
 /* Doc Block
- * Returns project title link from array content provided by database.
+ * Returns all project title links from array content provided by database.
  *
  * @param $array array associative array provided by database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string a list of project title links in the arrays within the arrays pulled from the database.
  */
 function returnProjectTitleLinks(array $array): string{
     $titlesLinks = '';
@@ -56,11 +56,11 @@ function returnProjectTitleLinks(array $array): string{
 }
 
 /* Doc Block
- * Returns project image link from array content provided by database.
+ * Returns all project image links from array content provided by database.
  *
  * @param $array array associative array provided by database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string a list of project image links in the arrays within the arrays pulled from the database.
  */
 function returnProjectImages(array $array): string{
     $projectImages = '';
@@ -71,11 +71,11 @@ function returnProjectImages(array $array): string{
 }
 
 /* Doc Block
- * Returns project image link from array content provided by database.
+ * Returns project id's from array content provided by database.
  *
  * @param $array array associative array provided by database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string a list of id's or keys in the arrays within the arrays pulled from the database.
  */
 function returnProjectIds(array $array): string{
     $projectIds = '';
@@ -86,11 +86,12 @@ function returnProjectIds(array $array): string{
 }
 
 /* Doc Block
- * Returns project titles from array content provided by database.
+ * Returns a single project title from array content provided by database.
  *
- * @param $array array associative array provided by database.
+ * @param $array array associative array provided by database query.
+ * @param $recId int number pointing to the record id number in the database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string the title content for a single record.
  */
 function returnProjectTitle(array $array, int $recId): string{ // change title to record
     $index = $recId - 1;
@@ -103,9 +104,10 @@ function returnProjectTitle(array $array, int $recId): string{ // change title t
 /* Doc Block
  * Returns project title link from array content provided by database.
  *
- * @param $array array associative array provided by database.
+ * @param $array array associative array provided by database query.
+ * @param $recId int number pointing to the record id number in the database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string the title link content for a single record.
  */
 function returnProjectTitleLink(array $array, int $recId): string{
     $index = $recId - 1;
@@ -118,9 +120,10 @@ function returnProjectTitleLink(array $array, int $recId): string{
 /* Doc Block
  * Returns project image link from array content provided by database.
  *
- * @param $array array associative array provided by database.
+ * @param $array array associative array provided by database query.
+ * @param $recId int number pointing to the record id number in the database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string the project image link for a single record.
  */
 function returnProjectImage(array $array, int $recId): string{
     $index = $recId - 1;
@@ -131,11 +134,12 @@ function returnProjectImage(array $array, int $recId): string{
 }
 
 /* Doc Block
- * Returns project image link from array content provided by database.
+ * Returns project id from array content provided by database.
  *
  * @param $array array associative array provided by database.
+ * @param $recId int number pointing to the record id number in the database.
  *
- * @return string the content of the arrays within the arrays pulled from the database.
+ * @return string an id or key for a single record.
  */
 function returnProjectId(array $array, int $recId){
     $index = $recId - 1;
