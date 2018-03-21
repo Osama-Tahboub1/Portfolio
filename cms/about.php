93dbd7ef31d28c87331140ede1cdb2e9ef2edc1c
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-03-21 19:06:48 +0000

    amended broken function, and added line breaks to another

--- cms/about.php
+++ cms/about.php
@@ -3,28 +3,53 @@
 $db = new PDO('mysql:host=127.0.0.1; dbname=osamasCMSDB', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
+//function getAboutMeArticle($db){
+//    $aboutMeArticleQuery = $db->prepare("SELECT `aboutMeArticle` FROM `aboutMe`;");
+//    $aboutMeArticleQuery->execute();
+//    return $aboutMeArticleQuery->fetchAll();
+//}
+//
+//$var = getAboutMeArticle($db);
+
+
 $aboutMeArticleQuery = $db->prepare("SELECT `aboutMeArticle` FROM `aboutMe`;");
 $aboutMeArticleQuery->execute();
 $aboutMeArticle = $aboutMeArticleQuery->fetchAll();
 
-$paragraphNumber = 1;
+$paragraphNumber =1;
 $articleParagraphQuery = $db->prepare("SELECT `aboutMeArticle` FROM `aboutMe` WHERE `id` = :paragraphNumber;");
 $articleParagraphQuery->bindParam (':paragraphNumber',$paragraphNumber);
 $articleParagraphQuery->execute();
 $articleParagraph = $articleParagraphQuery->fetchAll();
 
 /* Doc Block
- * Returns paragraph from array content provided by database.
+ * Returns article from array content provided by database.
  *
  * @param $array array associative array provided by database.
  *
  * @return string the content of the arrays within the arrays pulled from the database.
  */
-function returnParagraph(array $array): string{
-    $paragraph = '';
+function returnArticle(array $array): string{
+    $article = '';
+
     foreach ($array as $value) {
-        $paragraph .= $value['aboutMeArticle'];
+        $article .= $value['aboutMeArticle']."\n"."\n";
     }
+    return $article;
+}
+
+/* Doc Block
+ * Returns single paragraph from array content provided by database.
+ *
+ * @param $array array associative array provided by database.
+ *
+ * @return string the content of the arrays within the arrays pulled from the database.
+ */
+function returnParagraph(array $array, int $id): string{
+    $index = $id - 1;
+    $paragraphArray = $array[$index];
+    $paragraph = '';
+    $paragraph .= $paragraphArray['aboutMeArticle'];
     return $paragraph;
 }
 
@@ -41,30 +66,30 @@ function returnParagraph(array $array): string{
 
     <div>
         <form>
-            <textarea name="aboutMe" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($aboutMeArticle); ?></textarea>
+            <textarea name="aboutMe" cols="50" rows="15" maxlength="500"><?php echo returnArticle($aboutMeArticle); ?></textarea>
         </form>
     </div>
     <div>
-        <form method="post" action="admin.php">
-            <textarea name="paragraph1" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($articleParagraph); ?></textarea>
+        <textarea name="paragraph1" cols="50" rows="15" maxlength="500" form="paragraph1"><?php echo returnParagraph($aboutMeArticle, 1); ?></textarea>
+        <form method="post" action="admin.php" id="paragraph1">
             <input type="submit">
         </form>
     </div>
     <div>
         <form method="post" action="admin.php">
-            <textarea name="paragraph2" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($articleParagraph); ?></textarea>
+            <textarea name="paragraph2" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($aboutMeArticle, 2); ?></textarea>
             <input type="submit">
         </form>
     </div>
     <div>
         <form method="post" action="admin.php">
-            <textarea name="paragraph3" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($articleParagraph); ?></textarea>
+            <textarea name="paragraph3" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($aboutMeArticle, 3); ?></textarea>
             <input type="submit">
         </form>
     </div>
     <div>
         <form method="post" action="admin.php">
-            <textarea name="paragraph4" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($articleParagraph); ?></textarea>
+            <textarea name="paragraph4" cols="50" rows="15" maxlength="500"><?php echo returnParagraph($aboutMeArticle, 4); ?></textarea>
             <input type="submit">
         </form>
     </div>
