11841fe2fda9806cded58d45df785de0164209ac
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-03-23 10:26:38 +0000

    added new functions file, and refactored it in about page

--- about.php
+++ about.php
@@ -3,48 +3,10 @@
 $db = new PDO('mysql:host=127.0.0.1; dbname=osamasCMSDB', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-$aboutMeArticleQuery = $db->prepare("SELECT `aboutMeArticle` FROM `aboutMe`;");
-$aboutMeArticleQuery->execute();
-$aboutMeArticle = $aboutMeArticleQuery->fetchAll();
+require_once 'functions.php';
+$aboutMeArticle = getAboutMeArticle($db);
+$footerContent = getFooterContent($db);
 
-$contactIconQuery = $db->prepare("SELECT `contactIcon` FROM `footer`");
-$contactIconQuery->execute();
-$contactIcon = $contactIconQuery->fetchAll();
-
-$smallContactIconQuery = $db->prepare("SELECT `smallContactIcon` FROM `footer`");
-$smallContactIconQuery->execute();
-$smallContactIcon = $smallContactIconQuery->fetchAll();
-
-$contactEmailQuery = $db->prepare("SELECT `contactEmail` FROM `footer`");
-$contactEmailQuery->execute();
-$contactEmail = $contactEmailQuery->fetchAll();
-
-$emailSubjectQuery = $db->prepare("SELECT `emailSubject` FROM `footer`");
-$emailSubjectQuery->execute();
-$emailSubject = $emailSubjectQuery->fetchAll();
-
-$adminLoginIconQuery = $db->prepare("SELECT `adminLoginIcon` FROM `footer`");
-$adminLoginIconQuery->execute();
-$adminLoginIcon = $adminLoginIconQuery->fetchAll();
-
-$adminLoginLinkQuery = $db->prepare("SELECT `adminLoginLink` FROM `footer`");
-$adminLoginLinkQuery->execute();
-$adminLoginLink = $adminLoginLinkQuery->fetchAll();
-
-/* Doc Block
- * Adds paragraph from array content provided by database.
- *
- * @param $array array associative array provided by database.
- *
- * @return string the content of the arrays within the arrays pulled from the database.
- */
-function insertParagraph(array $array): string{
-    $paragraph = '';
-    foreach ($array as $value) {
-        $paragraph .= '<p>'.$value['aboutMeArticle'].'</p>';
-    }
-    return $paragraph;
-}
 ?>
 
 <!DOCTYPE html>
@@ -85,16 +47,16 @@ function insertParagraph(array $array): string{
     <footer class="container">
         <div class="contactIcon">
             <span>Contact me</span>
-            <a href="mailto:<?php echo $contactEmail[0]['contactEmail'];?>?Subject=<?php echo $emailSubject[0]['emailSubject'];?>"></a>
-            <img src="<?php echo $contactIcon[0]['contactIcon'];?>" alt="Email me icon">
-            <a href="<?php echo $adminLoginLink[0]['adminLoginLink'];?>"></a>
-            <img src="<?php echo $adminLoginIcon[0]['adminLoginIcon'];?>">
+            <a href="mailto:<?php echo $footerContent[0]['contactEmail'];?>?Subject=<?php echo $emailSubject[0]['emailSubject'];?>"></a>
+            <img src="<?php echo $footerContent[0]['contactIcon'];?>" alt="Email me icon">
+            <a href="<?php echo $footerContent[0]['adminLoginLink'];?>"></a>
+            <img src="<?php echo $footerContent[0]['adminLoginIcon'];?>">
         </div>
 
         <div class="contactIconSmall">
             <span>Contact me</span>
-            <a href="mailto:<?php echo $contactEmail[0]['contactEmail'];?>?Subject=<?php echo $emailSubject[0]['emailSubject'];?>"></a>
-            <img src="<?php echo $smallContactIcon[0]['smallContactIcon'];?>" alt="Email me icon">
+            <a href="mailto:<?php echo $footerContent[0]['contactEmail'];?>?Subject=<?php echo $emailSubject[0]['emailSubject'];?>"></a>
+            <img src="<?php echo $footerContent[0]['smallContactIcon'];?>" alt="Email me icon">
         </div>
     </footer>
 </body>
---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Returns an associative array with content from aboutMeArticle table from database.
+ *
+ * @param $db PDO database link and credentials PDO object.
+ *
+ * @return array returns associative array with arrays containing content from aboutMe table from database.
+ */
+function getAboutMeArticle(PDO $db):array {
+    $aboutMeArticleQuery = $db->prepare("SELECT `aboutMeArticle` FROM `aboutMe`;");
+    $aboutMeArticleQuery->execute();
+    $aboutMeArticle = $aboutMeArticleQuery->fetchAll();
+    return $aboutMeArticle;
+}
+
+/*
+ * Returns an associative array with content from footer table from database.
+ *
+ * @param $db PDO database link and credentials PDO object.
+ *
+ * @return array returns associative array with arrays containing content from footer table from database.
+ */
+function getFooterContent(PDO $db):array {
+    $footerContentQuery = $db->prepare("SELECT `contactIcon`, `smallContactIcon`, `contactEmail`, `emailSubject`, `adminLoginIcon`, `adminLoginLink`   FROM `footer`");
+    $footerContentQuery->execute();
+    $footerContent = $footerContentQuery->fetchAll();
+    return $footerContent;
+}
+
+/*
+ * Adds paragraph from array content provided by database.
+ *
+ * @param $array array associative array provided by database.
+ *
+ * @return string the text content of an array pulled from the database.
+ */
+function insertParagraph(array $array):string {
+    $paragraph = '';
+    foreach ($array as $value) {
+        $paragraph .= '<p>'.$value['aboutMeArticle'].'</p>';
+    }
+    return $paragraph;
+}
