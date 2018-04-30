f60d8f6fe889aa82ddef7643add13c0598e61a96
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-04-30 09:02:05 +0100

    removed white speace

--- portfolio.php
+++ portfolio.php
@@ -3,8 +3,6 @@
 $db = new PDO('mysql:host=127.0.0.1; dbname=osamasCMSDB', 'root');
 $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
-
-
 require_once 'functions.php';
 $footerContent = getFooterContent($db);
 $projectContent = getProjectContent($db);
