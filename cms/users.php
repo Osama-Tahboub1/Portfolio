a1df3726b98a692aece433474c9b92706769551b
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-13 14:52:26 +0100

    added password encryption functionality

--- cms/users.php
+++ cms/users.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: academy
- * Date: 13/05/2018
- * Time: 11:35
- */
+
+$db = new PDO('mysql:host=127.0.0.1; dbname=osamasCMSDB', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+require_once 'functions.php';
+
+$actualUserName = "";
+$actualPassword = "";
+
+$hashedPassword = password_hash($actualPassword, PASSWORD_DEFAULT);
+
+updateCredentials($db, $actualUserName, $hashedPassword);
+header('Location: login.php');
+
