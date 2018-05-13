417c3f19e760865c7c89f8402b563357d0dcd222
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-13 11:28:15 +0100

    removed page html as signouy function was updated to head to another page

--- cms/signOut.php
+++ cms/signOut.php
@@ -2,15 +2,3 @@
 require_once 'functions.php';
 signOut();
 
-?>
-
-<html<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Sign Out</title>
-</head>
-<body>
-    <span>Log back in </span><a href="login.php">here</a>
-</body>
-</html>
