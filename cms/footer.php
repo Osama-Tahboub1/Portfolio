d6664f60734440cb83549ca3d8d098a840fc122b
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-13 15:02:18 +0100

    added new file with connection to database, and with functions required

---
+++ cms/footer.php
@@ -0,0 +1,8 @@
+<?php
+$db = new PDO('mysql:host=127.0.0.1; dbname=osamasCMSDB', 'root');
+$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+
+require_once 'functions.php';
+
+?>
+
