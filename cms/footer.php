df77ab31ab7abbdf9ac67653eedc823cbfc09e3f
“Osama-Tahboub1” <o.tahboob2000@gmail.com>
2018-05-13 15:07:34 +0100

    added form for updating footer links

--- cms/footer.php
+++ cms/footer.php
@@ -6,3 +6,31 @@ require_once 'functions.php';
 
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Content Management System</title>
+</head>
+<body>
+<div>
+
+</div>
+
+
+<div>
+    <h2>Update Footer Links</h2>
+    <form method="post" action="footer.php">
+        <p>Email</p>
+        <textarea name="emailLink" cols="50" rows="2" maxlength="500"></textarea>
+        <p></p>
+        <input type="submit" value="Update">
+    </form>
+    <form method="post" action="footer.php">
+        <p>Github Account Link</p>
+        <textarea name="githubLink" cols="50" rows="2" maxlength="500"></textarea>
+        <p></p>
+        <input type="submit" value="Update">
+    </form>
+</div>
+</body>
